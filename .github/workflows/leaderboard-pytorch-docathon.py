f049269a5232a7cf636b65e9a8d98eaed60de937
Svetlana Karslioglu <svekars@fb.com>
2023-05-24 09:58:30 -0700

    Update leaderboard-pytorch-docathon.py

--- .github/workflows/leaderboard-pytorch-docathon.py
+++ .github/workflows/leaderboard-pytorch-docathon.py
@@ -30,9 +30,12 @@ def get_pull_requests(owner, repo):
         "labels": "easy,medium,advanced",
         "per_page": 100,
     }
-    response = requests.get(url.format(owner=owner, repo=repo), headers=headers, params=params)
-    pull_requests = response.json()
-    merged_pull_requests = [pr for pr in pull_requests if pr.get("merged_at")]
+    merged_pull_requests = []
+    for page in range(1, 6): #adding pagination to bypass 100 requests limitation of GitHub API
+        params["page"] = page
+        response = requests.get(url.format(owner=owner, repo=repo), headers=headers, params=params)
+        pull_requests = response.json()
+        merged_pull_requests += [pr for pr in pull_requests if pr.get("merged_at")]
     return merged_pull_requests
 
 author_data = {}
@@ -75,7 +78,7 @@ with open(csv_filename, "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(csv_data)
 
-print(f"Leaderoard saved to {csv_filename}")
+print(f"Leaderboard saved to {csv_filename}")
 
 markdown_table = "| Author | Points | PR |\n"
 markdown_table += "|--- | --- | ---|\n"
@@ -89,4 +92,4 @@ markdown_filename = "leaderboard-pytorch-docathon.md"
 with open(markdown_filename, "w", newline="") as file:
     file.write(markdown_table)
 
-print(f"Leaderoard saved to {markdown_filename}")
+print(f"Leaderboard saved to {markdown_filename}")
