f939a65bc79369e58c7d0bef14cdfa207abf77de
Svetlana Karslioglu <svekars@fb.com>
2023-05-25 13:48:51 -0700

    Update label_sync.py

--- .github/workflows/label_sync.py
+++ .github/workflows/label_sync.py
@@ -1,12 +1,11 @@
 import os
-import sys
 from github import Github
 
 token = os.environ['GITHUB_TOKEN']
 
 g = Github(token)
 
-pr_number = sys.argv[1]
+pr_number = os.environ['PR_NUMBER']
 
 repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
 pr = repo.get_pull(int(pr_number))
