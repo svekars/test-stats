ef16fd8ff19f5f398729ae154af8f854f916f427
Svetlana Karslioglu <svekars@fb.com>
2023-05-25 13:29:22 -0700

    Update label_sync.py

--- .github/workflows/label_sync.py
+++ .github/workflows/label_sync.py
@@ -1,16 +1,17 @@
 import os
+import sys
 from github import Github
 
 token = os.environ['GITHUB_TOKEN']
 
 g = Github(token)
 
-pr_number = os.environ['{{ github.event.pull_request.number }}']
-repo = g.get_repo('{{ github.repository }}')
-pr = repo.get_pull(pr_number)
+pr_number = sys.argv[1]
+
+repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
+pr = repo.get_pull(int(pr_number))
 
 issue_number = pr.title.split('#')[-1].strip()
-
 issue = repo.get_issue(issue_number)
 
 labels = [label.name for label in issue.labels]
